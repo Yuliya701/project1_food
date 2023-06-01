976601c7ed83897a1d65f114692aaa41c2cc15a0
Yuliya <yuliya.demchenko701@gmail.com>
2023-06-01 22:37:35 +0300

    modules added

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+
+import tabs from './modules/tabs';
+import calculation from './modules/calculation';
+import cards from './modules/forms';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import sliders from './modules/sliders';
+import timer from './modules/timer';
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+
+    tabs();
+    calculation();
+    cards();
+    forms();
+    modal();
+    sliders();
+    timer();
+});
+
+
+
